76d18032ef4357c9ca5342e14b3fc7d2d1c663e6
Mahamed Hassan <84216239+mhassan04@users.noreply.github.com>
2024-10-15 17:07:17 -0400

    Add buttons for grid resizing and reset, improve CSS layout.

--- script.js
+++ script.js
@@ -1,12 +1,71 @@
+const buttonsContainer = document.createElement("div");
+buttonsContainer.classList.add("buttons-container");
 
-function grids(){
-    const container = document.getElementById("container");
-    for (let i = 0; i < (16 * 16); i++){ 
+
+const button = document.createElement("button");
+button.textContent = "Change Grid Size";
+button.classList.add("buttons")
+button.addEventListener("click", function(){
+    try {
+        let selectGridSize = prompt("Enter the preferred grid size? (max: 100)");
+        selectGridSize = Number(selectGridSize);
+        if((!Number.isInteger(selectGridSize)) || selectGridSize < 1 || selectGridSize > 100){
+            alert("Please enter a number between 1 and 100");
+            return;
+        } 
+        clearGrid();  
+        grids(selectGridSize);
+    } catch (error){
+        console.error("An error occurred: ", error);
+        alert("An unexpected error occurred. Please try again.");
+    }
+});
+buttonsContainer.appendChild(button);
+
+const resetButton = document.createElement("button");
+resetButton.textContent = "Reset";
+resetButton.classList.add("buttons")
+resetButton.addEventListener("click", function(){
+    clearGrid();
+    grids(16);
+})
+buttonsContainer.appendChild(resetButton);
+
+const eraserButton = document.createElement("button");
+eraserButton.textContent = "Eraser";
+eraserButton.classList.add("buttons");
+eraserButton.addEventListener("click", function(){
+    
+})
+buttonsContainer.appendChild(eraserButton);
+
+document.body.appendChild(buttonsContainer);
+
+const container = document.createElement("div");
+container.setAttribute("id", "container");
+document.body.appendChild(container);
+
+function grids(n){
+    const containerSize = 640; 
+    container.style.width = `${containerSize}px`;
+    container.style.height = `${containerSize}px`;
+    const gridSquareSize = containerSize / n;
+
+    for (let i = 0; i < (n * n); i++){ 
         const gridSquare = document.createElement("div");
         gridSquare.classList.add("grid-square");
+        gridSquare.style.width = `${gridSquareSize}px`;
+        gridSquare.style.height = `${gridSquareSize}px`;
         container.appendChild(gridSquare);
     }
 }
 
-grids();
+function clearGrid(){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+}
+
+grids(16);
+
 
